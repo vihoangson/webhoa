fe4107e3ee2b4734ccad78473bc43772553c5b4c
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2017-03-30 20:34:59 +0700

    update

--- webhoa.sql
+++ webhoa.sql
@@ -15,26 +15,20 @@ CREATE DATABASE IF NOT EXISTS `webhoa` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `webhoa`;
 
 
--- Dumping structure for table webhoa.groups
-CREATE TABLE IF NOT EXISTS `groups` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` text COLLATE utf8_unicode_ci NOT NULL,
-  `parent_id` int(10) unsigned NOT NULL,
-  `sequence` int(10) unsigned NOT NULL,
+-- Dumping structure for table webhoa.group_post
+CREATE TABLE IF NOT EXISTS `group_post` (
+  `group_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `post_id` int(10) unsigned NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  PRIMARY KEY (`group_id`,`post_id`),
+  KEY `group_id_post_id` (`group_id`,`post_id`),
+  KEY `post_id` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table webhoa.groups: ~5 rows (approximately)
-/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` (`id`, `name`, `parent_id`, `sequence`, `created_at`, `updated_at`) VALUES
-	(1, 'asdafas222222222224124', 0, 0, '2017-03-30 11:22:07', '2017-03-30 13:04:16'),
-	(2, '424523433134', 0, 3, '2017-03-30 11:22:11', '2017-03-30 13:03:33'),
-	(3, 'ưerwer', 2, 4, '2017-03-30 12:02:06', '2017-03-30 13:03:44'),
-	(4, 'tưer21512541', 1, 2, '2017-03-30 12:02:09', '2017-03-30 13:04:20'),
-	(5, 'wrtwrt4124123', 1, 1, '2017-03-30 12:57:44', '2017-03-30 13:04:24');
-/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+-- Dumping data for table webhoa.group_post: ~0 rows (approximately)
+/*!40000 ALTER TABLE `group_post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `group_post` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
