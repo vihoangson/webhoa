136d0d0633caeb4e84fc897f654ce3c78dfd695f
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2017-04-24 06:18:10 +0700

    update

--- resources/views/public/hotdeal/includes/element/item1.blade.php
+++ resources/views/public/hotdeal/includes/element/item1.blade.php
@@ -9,14 +9,21 @@
             <p class="product-desciption">Praesent netus vitae hendrerit eu tellus nulla viverra</p>
             <div class="product-meta"><span class="product-time"><i
                             class="fa fa-clock-o"></i> 10 days 11 h remaining</span>
-                <ul class="product-price-list">
-                    <li><span class="product-price">{{$product->price_sale_formated}}</span>
-                    </li>
-                    <li><span class="product-old-price">{{$product->price_formated}}</span>
-                    </li>
-                    <li><span class="product-save">Save 57%</span>
-                    </li>
-                </ul>
+                @if($product->price_sale_formated)
+                    <ul class="product-price-list">
+                        <li><span class="product-price">{{$product->price_sale_formated}}</span>
+                        </li>
+                        <li><span class="product-old-price">{{$product->price_formated}}</span>
+                        </li>
+                        <li><span class="product-save">Save 64%</span>
+                        </li>
+                    </ul>
+                @else
+                    <ul class="product-price-list">
+                        <li><span class="product-price">{{$product->price_formated}}</span>
+                        </li>
+                    </ul>
+                @endif
             </div>
             <p class="product-location"><i class="fa fa-map-marker"></i> Boston</p>
         </div>
--- resources/views/public/hotdeal/includes/element/item3.blade.php
+++ resources/views/public/hotdeal/includes/element/item3.blade.php
@@ -9,14 +9,22 @@
             <p class="product-desciption">Tortor parturient id fermentum dictum enim maecenas
                 curabitur</p>
             <div class="product-meta"><span class="product-time"><i class="fa fa-clock-o"></i> 2 days 7 h remaining</span>
-                <ul class="product-price-list">
-                    <li><span class="product-price">{{$product->price_sale_formated}}</span>
-                    </li>
-                    <li><span class="product-old-price">{{$product->price_formated}}</span>
-                    </li>
-                    <li><span class="product-save">Save 64%</span>
-                    </li>
-                </ul>
+                @if($product->price_sale_formated)
+                    <ul class="product-price-list">
+                        <li><span class="product-price">{{$product->price_sale_formated}}</span>
+                        </li>
+                        <li><span class="product-old-price">{{$product->price_formated}}</span>
+                        </li>
+                        <li><span class="product-save">Save 64%</span>
+                        </li>
+                    </ul>
+                @else
+                    <ul class="product-price-list">
+                        <li><span class="product-price">{{$product->price_formated}}</span>
+                        </li>
+                    </ul>
+                @endif
+
             </div>
             <p class="product-location"><i class="fa fa-map-marker"></i> Boston</p>
         </div>
